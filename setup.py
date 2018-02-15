8bfbe7027bc05df2596946c9fabca75e33e20a4c
pick83 <dominik.strebel@empa.ch>
2018-02-15 12:21:26 +0100

    Removed Testcode

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='COSMOPythonLib',
-      version='0.1',
+      version='0.2',
       description='COSMO Python Utilities',
       url='http://github.com/pick250/COSMOPythonLib',
       author='Dominik Strebel',
       author_email='dominik.strebel@empa.ch',
       license='MIT',
       packages=['COSMOPythonLib','COSMOPythonLib.proj'],
-      install_requires=['numpy>0.8'],
+      install_requires=['numpy>0.8','netCDF4','pandas>0.15'],
       zip_safe=False)
